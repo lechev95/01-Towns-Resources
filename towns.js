e02777eb5876af693fbdf9d4bb3dc99e501cde37
llechev1 <162055944+llechev1@users.noreply.github.com>
2024-03-03 22:23:05 +0200

    Implemented functionality to shuffle the existing towns

--- towns.js
+++ towns.js
@@ -34,19 +34,20 @@ function showMessage(msg) {
     }, 3000);
 }
 
-function shuffleTowns() {
-    let towns = $('#towns option').toArray();
-    $('#towns').empty();
-    shuffleArray(towns);
-    $('#towns').append(towns);
-    $('#result').text("Towns shuffled.");
 
-    function shuffleArray(array) {
-        for (var i = array.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var oldElement = array[i];
-            array[i] = array[j];
-            array[j] = oldElement;
-        }
-    }
+function shuffleTowns() {
+	let towns = $('#towns option').toArray();
+	$('#towns').empty();
+	shuffleArray(towns);
+	$('#towns').append(towns);
+	$('#result').text("Towns shuffled.");
+
+	function shuffleArray(array) {
+		for (var i = array.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var oldElement = array[i];
+			array[i] = array[j];
+			array[j] = oldElement;
+		}
+	}
 }
